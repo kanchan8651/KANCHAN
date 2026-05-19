71036f0261a05aa078dc474716bfd424e78112e5
kanchan8651 <kanchanpramanik895@gmail.com>
2026-05-19 13:36:31 +0530

    function

---
+++ day 2/function2.js
@@ -0,0 +1,12 @@
+function isdivisible(a,b){
+    if(a%b==0){
+        console.log("yes");
+    }
+    else{
+        console.log("no");
+
+    }
+}
+isdivisible(31,10);
+isdivisible(0,10);
+isdivisible(6,10);
---
+++ day 2/son.js
@@ -0,0 +1,10 @@
+function sum(num) {
+  let count = 0;
+  for (let i = 0; i <= num; i++) {
+    // count = count + i;
+    count += i;
+  }
+  console.log(count);
+}
+
+sum(10);
