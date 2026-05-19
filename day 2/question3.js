6448124eb414a9da0e31fdc5354a624f70f99db3
kanchan8651 <kanchanpramanik895@gmail.com>
2026-05-20 00:33:51 +0530

    question3

--- day 2/question3.js
+++ day 2/question3.js
@@ -1,15 +1,16 @@
+
 for( let i=1;i<100;i++){
     if(i%3==0){
         console.log("fizz")
     
     }
-if(i%5==0){
+else if(i%5==0){
     console.log("buzz")
 }
-if(i%3==0$$i%5==0){
+else if(i%3==0&&i%5==0){
     console.log("fizzbuzz")
 }
 else{
-    console.log("  something went wrong   ")
+    console.log(" i ")
 }
 }
